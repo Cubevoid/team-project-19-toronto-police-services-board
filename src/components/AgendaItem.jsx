f50f0697db5958bf4aa079325a7a2644fba7ad4a
Clifton Sahota <clifton.sahota@mail.utoronto.ca>
2021-10-28 13:39:43 -0400

    Display richtext properly now

--- src/components/AgendaItem.jsx
+++ src/components/AgendaItem.jsx
@@ -50,9 +50,9 @@ export default class AgendaItem extends React.Component{
       <div>
         <div>AgendaId: {item.agenda}</div>
         <div>Title: {item.title}</div>
-        <div>{item.description}</div>
+        <div> {Parser(DOMPurify.sanitize(item.description))}</div>
         <div>Result: {item.result}</div>
-        <div>Motion: {Parser(DOMPurify.sanitize(item.notes))}</div>
+        <div>Motion: {Parser(DOMPurify.sanitize(item.motion))}</div>
         <a className="download-attach" href={item.file} download>{item.file ? item.file.split('/').pop() : item.file}</a>
         <br></br>
       </div>
