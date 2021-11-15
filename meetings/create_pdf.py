a36073fe36884753c5412622677dcce36aff3529
Cubevoid <52008334+Cubevoid@users.noreply.github.com>
2021-11-15 00:26:58 -0500

    Add basic table of contents

--- meetings/create_pdf.py
+++ meetings/create_pdf.py
@@ -8,6 +8,9 @@ from tpsb.settings import TIME_ZONE, BASE_DIR
 AUTHOR = 'Toronto Police Services Board'
 MARGIN_SIZE_MM = 25.4
 LETTER_WIDTH_MM = 8.5 * 25.4
+INDENT_AMOUNT = 8
+LINE_SPACING = 8
+FONT_FAMILY = 'Arial'
 
 
 def generate_agenda(agenda: Agenda, agenda_items: List[AgendaItem]):
@@ -21,8 +24,8 @@ def generate_agenda(agenda: Agenda, agenda_items: List[AgendaItem]):
     pdf.set_agenda(agenda)
     pdf.set_agenda_items(agenda_items)
     pdf.set_author(AUTHOR)
-    # pdf.print_chapter(1, 'A RUNAWAY REEF', '20k_c1.txt')
     pdf.print_title_page()
+    pdf.print_contents()
     pdf.output('test.pdf', 'F')
 
 
@@ -32,6 +35,7 @@ class PDF(FPDF):
         self.title = str(agenda)
         self.set_subject(agenda.meeting.description)
         self.date = agenda.meeting.date
+        self.agenda = agenda
 
     def set_agenda_items(self, agenda_items):
         self.agenda_items = agenda_items
@@ -40,40 +44,69 @@ class PDF(FPDF):
         # Position at 1.5 cm from bottom
         self.set_y(-15)
         # Arial italic 8
-        self.set_font('Arial', 'I', 8)
+        self.set_font(FONT_FAMILY, 'I', 8)
         # Text color in gray
         self.set_text_color(128)
         # Page number
         self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'C')
 
-    def chapter_title(self, num, label):
-        # Arial 12
-        self.set_font('Arial', '', 12)
-        # Background color
-        self.set_fill_color(200, 220, 255)
-        # Title
-        self.cell(0, 6, 'Chapter %d : %s' % (num, label), 0, 1, 'L', 1)
-        # Line break
-        self.ln(4)
+    def __print_content(self, agenda_item: AgendaItem):
+        self.set_font(family=FONT_FAMILY, size=12)
 
-    def chapter_body(self, name):
-        # Read text file
-        with open(name, 'rb') as fh:
-            txt = fh.read().decode('latin-1')
-        # Times 12
-        self.set_font('Times', '', 12)
-        # Output justified text
-        self.multi_cell(0, 5, txt)
-        # Line break
-        self.ln()
-        # Mention in italics
-        self.set_font('', 'I')
-        self.cell(0, 5, '(end of excerpt)')
+        # Indent sub-items
+        if agenda_item.number % 1 != 0:
+            self.cell(w=INDENT_AMOUNT + self.get_string_width('1. '), h=LINE_SPACING)
 
-    def print_chapter(self, num, title, name):
+        num = f'{agenda_item.number}. '
+        self.cell(w=self.get_string_width(num), h=LINE_SPACING, txt=num)
+        self.cell(w=INDENT_AMOUNT, h=LINE_SPACING)
+
+        w = LETTER_WIDTH_MM
+        w -= 60 if agenda_item.number % 1 == 0 else INDENT_AMOUNT + 60
+        self.multi_cell(w=w, h=LINE_SPACING, txt=f'{agenda_item.title}\n{agenda_item.description}', align='L')
+
+    def print_contents(self):
         self.add_page()
-        self.chapter_title(num, title)
-        self.chapter_body(name)
+
+        # Arial bold 15
+        self.set_font(FONT_FAMILY, 'B', 16)
+        # Calculate width of title and position
+        w = self.get_string_width(self.title)
+        self.set_x((LETTER_WIDTH_MM - w) / 2)
+        # Title
+        self.cell(w=w, h=LINE_SPACING, txt=self.title, ln=2, align='C')
+
+        tz = timezone(TIME_ZONE)
+        time = self.date.astimezone(tz)
+        time_formatted = time.strftime("%A %B %d, %Y at %I:%M %p")
+        self.set_font(FONT_FAMILY, 'B', 12)
+        w = self.get_string_width(time_formatted)
+        self.set_x((LETTER_WIDTH_MM - w) / 2)
+        # Date and time
+        self.cell(w=w, h=LINE_SPACING, txt=time_formatted, ln=2, align='C')
+
+        if self.agenda.meeting.recording_link:
+            w = self.get_string_width(f'Livestream at: {self.agenda.meeting.recording_link}')
+            self.set_x((LETTER_WIDTH_MM - w) / 2)
+            # Non-link part
+            self.cell(w=self.get_string_width('Livestream at: '), h=LINE_SPACING, txt='Livestream at: ', align='C')
+            # Hyperlink part
+            self.set_text_color(r=0, g=0, b=255)
+            self.set_font(FONT_FAMILY, 'UB', 12)
+            self.cell(w=w,
+                      h=LINE_SPACING,
+                      txt=self.agenda.meeting.recording_link,
+                      ln=2,
+                      align='L',
+                      link=self.agenda.meeting.recording_link)
+            self.set_text_color(r=0)  # Reset to black
+        self.ln(LINE_SPACING / 2)
+        self.line(50, self.get_y(), LETTER_WIDTH_MM - 50, self.get_y())
+        self.ln(LINE_SPACING / 2)
+
+        for agenda_item in self.agenda_items:
+            self.__print_content(agenda_item)
+            self.ln(LINE_SPACING / 2)
 
     def print_title_page(self):
         self.add_page()
@@ -84,23 +117,23 @@ class PDF(FPDF):
         self.image(logo, x=x, w=width, h=width)
 
         # Arial bold 15
-        self.set_font('Arial', 'B', 16)
+        self.set_font(FONT_FAMILY, 'B', 16)
         # Calculate width of title and position
         w = self.get_string_width(self.title)
         self.set_y(width + 40)
         self.set_x((LETTER_WIDTH_MM - w) / 2)
         # Title
-        self.cell(w=w, txt=self.title, ln=2, align='C')
-        self.ln(10)
+        self.cell(w=w, txt=self.title, h=LINE_SPACING, ln=2, align='C')
+        self.ln(LINE_SPACING)
 
         tz = timezone(TIME_ZONE)
         time = self.date.astimezone(tz)
         time_formatted = time.strftime("%A %B %d, %Y at %I:%M %p")
-        self.set_font('Arial', 'B', 12)
+        self.set_font(FONT_FAMILY, 'B', 12)
         w = self.get_string_width(time_formatted)
         self.set_x((LETTER_WIDTH_MM - w) / 2)
         # Date and time
-        self.cell(w=w, h=9, txt=time_formatted, ln=2, align='C')
+        self.cell(w=w, h=LINE_SPACING, txt=time_formatted, ln=2, align='C')
 
 
 # https://stackoverflow.com/a/39327252/13176711
--- meetings/tests.py
+++ meetings/tests.py
@@ -10,8 +10,12 @@ class PDFGenerationTestCase(TestCase):
     meeting = Meeting(title='TPSB Nov Test Meeting',
                       date=datetime.now(),
                       description='Test Description',
-                      meeting_type='PUB')
+                      meeting_type='PUB',
+                      recording_link='https://youtube.com/')
     agenda = Agenda(meeting=meeting)
+    item1 = AgendaItem(agenda=agenda, number=1, title='Agenda Item 1', description='Test Description 1')
+    item11 = AgendaItem(agenda=agenda, number=1.1, title='Agenda Item 1.1', description='Test 1.1')
+    item2 = AgendaItem(agenda=agenda, number=2, title='Really Long Title ' * 8, description='Test Description 2')
 
     def test_basic_pdf(self):
-        create_pdf.generate_agenda(self.agenda, [])
+        create_pdf.generate_agenda(self.agenda, [self.item1, self.item11, self.item2])
