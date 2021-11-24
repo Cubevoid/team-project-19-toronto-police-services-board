de29680be89c497e66c93fe6cef4b02e518644cf
Cubevoid <52008334+Cubevoid@users.noreply.github.com>
2021-11-23 20:39:26 -0500

    Add default agenda template fixture

--- reset.sh
+++ reset.sh
@@ -6,6 +6,6 @@ rm -f meetings/migrations/0*.py
 python manage.py makemigrations
 python manage.py migrate
 DJANGO_SUPERUSER_PASSWORD=admin python manage.py createsuperuser --username admin --email admin@example.com --noinput
-python manage.py loaddata admin_interface_theme_TPSB.json
+python manage.py loaddata meetings/fixtures/*.json
 
 rm -rf uploads
